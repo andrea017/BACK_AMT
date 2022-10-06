12b6e4346ded929385e7bf3deb43d0d899798977
andrea017 <mamolinatorres@gmail.com>
2022-10-06 00:33:37 -0300

    Update EducacionController.java

--- src/main/java/com/portfolio/amt/Controller/EducacionController.java
+++ src/main/java/com/portfolio/amt/Controller/EducacionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frontamt.web.app/")
+@CrossOrigin(origins = "https://frontamt.web.app")
 public class EducacionController {
 
     @Autowired
