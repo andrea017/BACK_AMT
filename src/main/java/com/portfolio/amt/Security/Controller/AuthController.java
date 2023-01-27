545f8a389f76a16af591a549abd396c4e2639305
andrea017 <mamolinatorres@gmail.com>
2023-01-27 18:00:38 -0300

    cambios de permisos front

--- src/main/java/com/portfolio/amt/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/amt/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontamt.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://amt-ap.web.app", "http://localhost:4200"})
 public class AuthController {
 
     @Autowired
